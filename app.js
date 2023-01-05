a88dfc1501141df08f54b99eaeddbf2dd88960fb
unknown <gachi-ferrari@hotmail.com>
2023-01-05 15:56:37 -0300

    agregado de media queries

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const $contenedor = document.getElementById("contenedor");
 
-const URL_API="https://rickandmortyapi.com/api/character";
+const URL_API = "https://rickandmortyapi.com/api/character";
 
 fetch(URL_API)
-.then((response) => response.json())
-.then((data) => {
-    const personajes = data.results;
+    .then((response) => response.json())
+    .then((data) => {
+        const personajes = data.results;
 
 
-for(let character of personajes){
-    $contenedor.innerHTML +=`
+        for (let character of personajes) {
+            $contenedor.innerHTML += `
     <div class="cajon"> <img class="img-personaje" src="${character.image}" alt="imagen de ${character.name}">
     <h2 class="nombre">Nombre: ${character.name}</h2>
     <h3 class="especie">Especie: ${character.species}</h3>
     <h3 class="estado">Estado: ${character.status}</h3>
     </div>
     `;
-}
-});
+        }
+    });
